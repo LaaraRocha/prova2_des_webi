302885d2b43890c2ec2a6ff78c228346e2171e32
LaaraRocha <laara.rocha@unidavi.edu.br>
2021-11-03 19:59:32 -0300

    Segundo

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+$n1 = isset($_GET['numero1']) ? $_GET['numero1']*1 : 0 ;
+$n2 = isset($_GET['numero2']) ? $_GET['numero2']*1 : 0 ;
+$op = isset($_GET['escolha']) ? $_GET['escolha'] : 0 ;
+
+switch ($op){
+
+    case 'Soma':
+        $result = $n1 + $n2;
+        echo "O Resultado é: $result";
+        break;
+
+    case 'Subtração':
+        $result = $n1 - $n2;
+        echo "O Resultado é: $result";
+        break;
+
+    default: # adicionado
+        echo "Sem resultado";
+}
