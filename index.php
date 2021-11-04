028e90aeaa5b6a708fe92f6adedc62da2f1c9d63
Laara <laara.rocha@unidavi.edu.br>
2021-11-03 21:41:09 -0300

    ultimo commit

--- index.php
+++ index.php
@@ -15,9 +15,51 @@ switch ($op){
         echo "O Resultado é: $result";
         break;
 
-    default: # adicionado
+    default:
         echo "Sem resultado";
 }
+if (isset($_POST['num'])) {
+    if (!isset($_SESSION['num1'])) {
+        $_SESSION['num1'] = $_POST['num'];
+    } else {
+        $_SESSION['num2'] = $_POST['num'];
+    }
+
+if (isset($_POST['operator'])) {
+    $_SESSION['operator'] = $_POST['operator'];
+}
+
+if (isset($_POST['equal'])) {
+    if ($_SESSION['operator'] == '+'){
+     $_SESSION['result'] = $_SESSION['num1'] + $_SESSION['num2'];
+    }
+}
+
+if (isset($_POST['equal'])) {
+    if ($_SESSION['operator'] == '-'){
+        $_SESSION['result'] = $_SESSION['num1'] - $_SESSION['num2'];
+    }
+}
+
+    if (isset($_POST['equal'])) {
+        if ($_SESSION['operator'] == 'x'){
+            $_SESSION['result'] = $_SESSION['num1'] * $_SESSION['num2'];
+        }
+    }
+
+    if (isset($_POST['equal'])) {
+        if ($_SESSION['operator'] == '/'){
+            $_SESSION['result'] = $_SESSION['num1'] / $_SESSION['num2'];
+        }
+    }
+
+if (isset($_POST['equal'])) {
+    echo ['rsult'];
+}
+
+}
+
+
 ?>
 
 
@@ -28,76 +70,76 @@ switch ($op){
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
-    <div>
-        <table class="result">
-            <th>
+<div>
+    <table class="result">
+        <th>
 
-            </th>
-        </table>
-    </div>
-    <form>
-        <table>
-            <tr>
-                <td>
-                    <input type="button" id="7" name="n_7" value="7">
-                </td>
-                <td>
-                    <input type="button" id="8" name="n_8" value="8">
-                </td>
-                <td>
-                    <input type="button" id="9" name="n_9" value="9">
-                </td>
-                <td>
-                    <input type="button" id="somar" name="somar" value="+">
-                </td>
-            </tr>
+        </th>
+    </table>
+</div>
+<form>
+    <table>
+        <tr>
+            <td>
+                <input type="button" name="num" value="7">
+            </td>
+            <td>
+                <input type="button" name="num" value="8">
+            </td>
+            <td>
+                <input type="button" name="num" value="9">
+            </td>
+            <td>
+                <input type="button" name="operator" value="+">
+            </td>
+        </tr>
 
-            <tr>
-                <td>
-                    <input type="button" id="4" name="n_4" value="4">
-                </td>
-                 <td>
-                     <input type="button" id="5" name="n_5" value="5">
-                </td>
-                <td>
-                    <input type="button" id="6" name="n_6" value="6">
-                </td>
-                <td>
-                    <input type="button" id="subtrair" name="subtrair" value="- ">
-                </td>
-            </tr>
+        <tr>
+            <td>
+                <input type="button" name="num" value="4">
+            </td>
+            <td>
+                <input type="button" name="num" value="5">
+            </td>
+            <td>
+                <input type="button" name="num" value="6">
+            </td>
+            <td>
+                <input type="button" name="operator" value="-">
+            </td>
+        </tr>
 
-            <tr>
-                 <td>
-                    <input type="button" id="1" name="n_1" value="1">
-                 </td>
-                <td>
-                    <input type="button" id="2" name="n_2" value="2">
-                </td>
-                <td>
-                    <input type="button" id="3" name="n_3" value="3">
-                </td>
-                <td>
-                    <input type="button" id="multi" name="multiplicar" value="x">
-                </td>
-            </tr>
+        <tr>
+            <td>
+                <input type="button" name="num" value="1">
+            </td>
+            <td>
+                <input type="button" name="num" value="2">
+            </td>
+            <td>
+                <input type="button" name="num" value="3">
+            </td>
+            <td>
+                <input type="button" name="operator" value="x">
+            </td>
+        </tr>
 
-            <tr>
-                <td>
-                    <input type="button" id="0" name="n_0" value="0">
-                </td>
-                <td>
-                    <input type="reset" id="limpar" name="limpar" value="c">
-                </td>
-                <td>
-                    <input type="submit" id="igual" name="igual" value="=">
-                </td>
-                <td>
-                    <input type="button" id="dividir" name="dividir" value="÷">
-                </td>
-            </tr>
-        </table>
-    </form>
+        <tr>
+            <td>
+                <input type="button" name="num" value="0">
+            </td>
+            <td>
+                <input type="reset" name="limpar" value="c">
+            </td>
+            <td>
+                <input type="submit" name="equal" value="=">
+            </td>
+            <td>
+                <input type="button" name="operator" value="/">
+            </td>
+        </tr>
+    </table>
+</form>
 
 </body>
 </html>
